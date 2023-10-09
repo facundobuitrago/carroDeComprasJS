b60f43d85d3440c7900c76f1ce24fc72111eeb29
unknown <facundo buitrago>
2023-10-09 19:23:52 -0300

    descuento

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
 // Este código de JavaScript permite a un usuario seleccionar productos y agregarlos a un carrito de compras a través de un proceso interactivo. Luego, el usuario puede elegir el método de pago (efectivo o tarjeta) y se muestra el detalle de la compra, incluyendo el total a pagar y cualquier descuento aplicado. El programa permite al usuario realizar múltiples compras.
 
-
-let saludo = alert("Bienvenido a la Cerrajeria Online! a continuación podra elegir las llaves a copiar ");
-  
-  const productos =  [
-    {nombre: "Llave Yale", precio: 1500},
-    {nombre: "Llave Doble Paleta", precio: 1500},
-    {nombre: "Llave Computada", precio: 3500},
-    {nombre: "Llave Cruz", precio: 2000},
-    {nombre: "Llave Auto", precio: 4500},
- ]
+let saludo = alert(
+  "Bienvenido a la Cerrajería Online! A continuación, podrá elegir las llaves a copiar."
+);
 
 function realizarCompra() {
-  const carrito = [];
   let total = 0;
 
   while (true) {
-    let opcionesProductos = "Productos disponibles:\n";
-    for (let i = 0; i < productos.length; i++) {
-      opcionesProductos += `${i + 1}. ${productos[i].nombre} - $${productos[i].precio}\n`;
-    }
+    let opcionesProductos =
+      "Productos disponibles:\n" +
+      "1. Llave Yale - $1500\n" +
+      "2. Llave Doble Paleta - $1500\n" +
+      "3. Llave Computada - $3500\n" +
+      "4. Llave Cruz - $2000\n" +
+      "5. Llave Auto - $4500\n";
 
-    const seleccion = prompt(opcionesProductos + "Seleccione el modelo de llave (ingrese el número) o escriba 'pagar' para finalizar la compra:");
+    const seleccion = prompt(
+      opcionesProductos +
+        "Seleccione el modelo de llave (ingrese el número) o escriba 'pagar' para finalizar la compra:"
+    );
 
     if (seleccion.toLowerCase() === "pagar") {
-      if (carrito.length === 0) {
+      if (total === 0) {
         alert("El carrito está vacío. Seleccione algún modelo de llave.");
-        continue; 
+        continue;
       }
       break;
     }
 
-    const indice = parseInt(seleccion) - 1;
+    let precioProducto = 0;
 
-    if (indice >= 0 && indice < productos.length) {
-      carrito.push(productos[indice]);
-      total += productos[indice].precio;
-      alert(`"${productos[indice].nombre}" se ha añadido al carrito.`);
-    } else {
-      alert("Producto no válido. Por favor, seleccione un número válido.");
+    switch (seleccion) {
+      case "1":
+        precioProducto = 1500;
+        break;
+      case "2":
+        precioProducto = 1500;
+        break;
+      case "3":
+        precioProducto = 3500;
+        break;
+      case "4":
+        precioProducto = 2000;
+        break;
+      case "5":
+        precioProducto = 4500;
+        break;
+      default:
+        alert("Selección no válida. Por favor, ingrese un número válido.");
+        continue;
     }
+
+    total += precioProducto;
+    alert(`Llave seleccionada - $${precioProducto}.`);
   }
 
-  return { carrito, total };
+  return total;
 }
 
 function calcularDescuento(total, metodoPago) {
@@ -55,21 +69,13 @@ function calcularDescuento(total, metodoPago) {
   }
 }
 
-const compra = realizarCompra();
-const carrito = compra.carrito;
-const total = compra.total;
+const total = realizarCompra();
 const metodoPago = prompt("¿Desea pagar en efectivo o tarjeta?");
-const { total: totalConDescuento, descuento } = calcularDescuento(total, metodoPago);
+const { total: totalConDescuento, descuento } = calcularDescuento(
+  total,
+  metodoPago
+);
 
-
-
-let listaProductos = "Productos en el carrito:\n";
-for (const producto of carrito) {
-  listaProductos += `- ${producto.nombre} - $ ${producto.precio}\n`;
-}
-
-alert(listaProductos);
-alert(`Total de productos: ${carrito.length}`);
-alert(`Total a pagar: $ ${totalConDescuento}`);
-alert(`Descuento aplicado: $ ${descuento}`);
-alert(`Gracias por su compra`);
+alert(`Total a pagar: $${totalConDescuento}`);
+alert(`Descuento aplicado: $${descuento}`);
+alert("Gracias por su compra");
